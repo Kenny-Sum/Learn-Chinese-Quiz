02bccf0a1c0bb1cf167937ce82d301a10b0e6612
Kenny <keggex101@gmail.com>
2025-10-19 02:16:56 +0100

    Added a list of question, displayed flex to center text and placed buttons on page.

---
+++ script.js
@@ -0,0 +1,21 @@
+const questions = [
+    {
+        question: "what is the name of your first pet?",
+        answer: [
+            {text: "loki", correct: false},
+            {text: "cloud" , correct: false},
+            {text: "evie", correct: true},
+            {text: "jackie", correct: false}
+        ]
+    },
+    {
+        question: "How many pets do you have?",
+        answer: [
+            {text: "one", correct: true},
+            {text: "two" , correct: false},
+            {text: "three", correct: false},
+            {text: "four", correct: false}
+        ]
+    }
+]
+
