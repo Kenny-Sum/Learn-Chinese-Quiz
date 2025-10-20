f1e031e645807f2423d3e81f888a24fff4695b84
Kenny <keggex101@gmail.com>
2025-10-20 06:54:45 +0100

    created a dictionary "questionOne". With the first question and answers.

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+const questionOne = {
+    question : "Is the earth round, flat, square or rectangle?",
+    answer : [
+        { text: "round", correct: true},
+        { text: "flat", correct: false},
+        { text: "square", correct:false},
+        { text: "rectangle", correct:false}
+    ]
+};
+
 const element = document.getElementById("question_section");
-element.textContent = "Is the earth round, flat, square or rectangle?";
+element.textContent = questionOne.question;
 
 const elementAnswerOne = document.getElementById("answerOne");
-elementAnswerOne.textContent = "round";
+elementAnswerOne.textContent = questionOne.answer[0].text;
 
 const elementAnswerTwo = document.getElementById("answerTwo");
-elementAnswerTwo.textContent = "flat";
+elementAnswerTwo.textContent = questionOne.answer[1].text;
 
 const elementAnswerThree = document.getElementById("answerThree");
-elementAnswerThree.textContent = "square";
+elementAnswerThree.textContent = questionOne.answer[2].text;
 
 const elementAnswerFour = document.getElementById("answerFour");
-elementAnswerFour.textContent = "rectangle";
+elementAnswerFour.textContent = questionOne.answer[3].text;
 
 
 elementAnswerOne.addEventListener('click', function() {
@@ -34,6 +44,6 @@ elementAnswerThree.addEventListener('click', function() {
 
 elementAnswerFour.addEventListener('click', function() {
     alert("wrong");
-    const answer = document.getElementById("next_section");
-    answer.textContent = "wrong"
+    const nextSection = document.getElementById("next_section");
+    nextSection.textContent = "wrong"
 });
