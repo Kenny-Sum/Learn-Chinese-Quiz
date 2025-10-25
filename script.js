a390b8ec6695b0f8095f0d766c76e4f026243cfb
Kenny <keggex101@gmail.com>
2025-10-25 05:32:00 +0100

    Created a createBtn function that creates a heading showing right/wrong answer and a next btn.

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 // Create a buttom element.
-const buttom_element = document.getElementById("bottom_section");
+    const buttom_element = document.getElementById("bottom_section");
 
-// Create a new <button> element.
-const  button = document.createElement("button");
+// Create button function.
+function createBtn() {
+    // Creates a heading showing if the answer is right or wrong.
+    buttom_element.innerHTML = ""; // removes all html elements within container.
+    const heading = document.createElement("h3"); // Create a new <h3> element
+    heading.textContent = "Correct or Wrong"; // Writes the new heading with text.
+    buttom_element.appendChild(heading); // appends the heading to the bottom_element container. 
 
-// Make the button read 'Next'.
-button.textContent = "Next";
+    // Creates a button that has the inner text 'Next'.
+    const  button = document.createElement("button"); // Create a new <button> element.
+    button.textContent = "Next"; // Make the button read 'Next'.
+    button.id = "nextBtn";// Set the button id to nextBtn.
+    buttom_element.appendChild(button); // Append btn to the container button_section.
+}
 
-// Set the button id to nextBtn.
-button.id = "nextBtn";
+// selects all elements that matches the css selector 'answer'.
+const answers = document.querySelectorAll(".answer");
 
-// Append btn to the container button_section.
-document.getElementById("bottom_section").appendChild(button);
+// Selects all element that contains the class name 'answer'.
+answers.forEach((answer) => {
+    answer.addEventListener("click", createBtn); // adds a create btn element.
+});
+
 
