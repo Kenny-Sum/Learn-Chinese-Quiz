f19117dab74140699397090373aa91d4d20569c0
Kenny <keggex101@gmail.com>
2025-10-28 04:48:35 +0000

    Created a list containing dictionaries of key-value pairs that has the question, the options to choose from and the answer. Also Created the function start quiz that starts the quiz from the first question.

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-// Create a buttom element.
-    const buttom_element = document.getElementById("bottom_section");
+// questionOne Dictionary contains the question, answers and which answer is correct, which has been listed as a key-value pair for each.
+const questionOne = {
+    question : "what is 2 + 2? ", // This key value pair represents the question.
+    options : ["4", "5", "7", "9"], // This key value pair represents the options and list of answers.
+    answer : "4" // This represents which  is the answer, the key is the name 'answer' and the value is the actual answer.
+};
+
+// questionTwi Dictionary contains the question, answers and which answer is correct,  which has been listed as a key-value pair for each.
+const questionTwo = {
+    question : "When you mix colour yellow and blue, what do you get? ", // This key value pair represents the question.
+    options : ["pink", "black", "green", "white"], // This key value pair represents the options and list of answers. 
+    answer : "green" // This represents which index is the answer for the list of answers.
+};
+
+// List containing list of questions. 
+const questionList = [questionOne, questionTwo];
+
+// Create a buttom element linking to bottom section ID.
+const buttom_element = document.getElementById("bottom_section");
+
+// Create a question element linking to question section ID.
+const question_element = document.getElementById("question_section");
+
+// Create answerOne element linking to answerOne ID.
+const answerOne_element = document.getElementById("answerOne");
+
+// Create answerTwo element linking to answerOne ID.
+const answerTwo_element = document.getElementById("answerTwo");
+
+// Create answerThree element linking to answerOne ID.
+const answerThree_element = document.getElementById("answerThree");
+
+// Create answerFour element linking to answerOne ID.
+const answerFour_element = document.getElementById("answerFour");
+
 
 // Create button function.
 function createBtn() {
@@ -24,4 +57,21 @@ answers.forEach((answer) => {
     answer.addEventListener("click", createBtn); // adds a create btn element.
 });
 
+function startQuiz() {
+    let questionNum = 0;
 
+    question_element.innerHTML = ""; // removes all html element within question_section container.
+    const questionHeading = document.createElement("h3"); // create a new <h3> element.
+    question_element.appendChild(questionHeading); // appends the heading to the question_element container. 
+    questionHeading.textContent = questionList[questionNum].question; // Set the question container to match current question.  
+
+    answerOne_element.textContent = questionList[questionNum].options[0]; // Set the answer One container text to the first answer.
+    answerTwo_element.textContent = questionList[questionNum].options[1]; // Set the answer Two container text to the second answer.
+    answerThree_element.textContent = questionList[questionNum].options[2]; // Set the answer Three container text to the third answer.
+    answerFour_element.textContent = questionList[questionNum].options[3]; // Set the answer Four container text to the fourth answer.
+
+}
+
+startQuiz();
+
+console.log(questionList[0].question);
