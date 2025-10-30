f359f0bf10a646b2d438e6ef4dd4afc6cc590764
Kenny <keggex101@gmail.com>
2025-10-30 03:55:50 +0000

    added more questions and change the frist three.

--- script.js
+++ script.js
@@ -1,26 +1,68 @@
 // questionOne Dictionary contains the question, answers and which answer is correct, which has been listed as a key-value pair for each.
 const questionOne = {
-    question : "what is 2 + 2? ", // This key value pair represents the question.
-    options : ["4", "5", "7", "9"], // This key value pair represents the options and list of answers.
-    answer : "4" // This represents which  is the answer, the key is the name 'answer' and the value is the actual answer.
+    question : "你是哪__人?", // This key value pair represents the question.
+    options : ["你", "国", "名", "我"], // This key value pair represents the options and list of answers.
+    answer : "国" // This represents which  is the answer, the key is the name 'answer' and the value is the actual answer.
 };
 
 // questionTwo Dictionary contains the question, answers and which answer is correct,  which has been listed as a key-value pair for each.
 const questionTwo = {
-    question : "When you mix colour yellow and blue, what do you get? ", // This key value pair represents the question.
-    options : ["pink", "black", "green", "white"], // This key value pair represents the options and list of answers. 
-    answer : "green" // This represents which index is the answer for the list of answers.
+    question : "我是日本人, 你__?", // This key value pair represents the question.
+    options : ["美", "呢", "国", "叫"], // This key value pair represents the options and list of answers. 
+    answer : "呢" // This represents which index is the answer for the list of answers.
 };
 
 // questionThree Dictionary contains the question, answers and which answer is correct,  which has been listed as a key-value pair for each.
 const questionThree = {
-    question : "你是哪__人? ", // This key value pair represents the question.
-    options : ["你", "国", "green", "white"], // This key value pair represents the options and list of answers. 
-    answer : "国" // This represents which index is the answer for the list of answers.
+    question : "我__美国人。", // This key value pair represents the question.
+    options : ["朋友", "国", "叫", "是"], // This key value pair represents the options and list of answers. 
+    answer : "是" // This represents which index is the answer for the list of answers.
+};
+
+// questionThree Dictionary contains the question, answers and which answer is correct,  which has been listed as a key-value pair for each.
+const questionFour = {
+    question : "你叫___名字？。", // This key value pair represents the question.
+    options : ["你", "中国", "什么", "是"], // This key value pair represents the options and list of answers. 
+    answer : "什么" // This represents which index is the answer for the list of answers.
+};
+
+// questionThree Dictionary contains the question, answers and which answer is correct,  which has been listed as a key-value pair for each.
+const questionFive = {
+    question : "我__欧文。", // This key value pair represents the question.
+    options : ["学生", "国", "叫", "是"], // This key value pair represents the options and list of answers. 
+    answer : "叫" // This represents which index is the answer for the list of answers.
+};
+
+// questionThree Dictionary contains the question, answers and which answer is correct,  which has been listed as a key-value pair for each.
+const questionSix = {
+    question : "这是我的中国___。", // This key value pair represents the question.
+    options : ["朋友", "国", "那个", "是"], // This key value pair represents the options and list of answers. 
+    answer : "朋友" // This represents which index is the answer for the list of answers.
+};
+
+// questionThree Dictionary contains the question, answers and which answer is correct,  which has been listed as a key-value pair for each.
+const questionSeven = {
+    question : "我__王，___王山。", // This key value pair represents the question.
+    options : ["你", "国", "green", "姓, 我叫"], // This key value pair represents the options and list of answers. 
+    answer : "姓, 我叫" // This represents which index is the answer for the list of answers.
+};
+
+// questionThree Dictionary contains the question, answers and which answer is correct,  which has been listed as a key-value pair for each.
+const questionEight = {
+    question : "我__住留学生楼。", // This key value pair represents the question.
+    options : ["也", "国", "green", "是"], // This key value pair represents the options and list of answers. 
+    answer : "也" // This represents which index is the answer for the list of answers.
+};
+
+// questionThree Dictionary contains the question, answers and which answer is correct,  which has been listed as a key-value pair for each.
+const questionNine = {
+    question : "你住那个___？", // This key value pair represents the question.
+    options : ["你", "国", "房间", "是"], // This key value pair represents the options and list of answers. 
+    answer : "房间" // This represents which index is the answer for the list of answers.
 };
 
 // List containing list of questions. 
-const questionList = [questionOne, questionTwo, questionThree];
+const questionList = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight, questionNine];
 
 // Create a buttom element linking to bottom section ID.
 const buttom_element = document.getElementById("bottom_section");
